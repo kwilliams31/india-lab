28987a0874f905f84a695012150a6bebd2b92b15
Kristina <111464160+kwilliams31@users.noreply.github.com>
2022-11-21 11:25:39 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,3 @@
-// doctype,, putting $ before function, jq
-// using jq to get button to change and show when something else is selected
-// checkbox 
-
-$(function () {
-
-function myFunction() {
-    // Get the checkbox
-    var checkBox = document.getElementById("myCheck");
-    // Get the output text
-    var text = document.getElementById("submitButton");
-  
-    // If the checkbox is checked, display the output text
-    if (checkBox.checked == true) {
-      text.style.display = "block";
-    } else {
-      text.style.display = "none";
-    }
-  }
-
 
   // function myFunction() {
   //   // Get the checkbox
@@ -52,25 +32,49 @@ function myFunction() {
 //  ]
 
 
+let userData = ["email", "password"];
+  let userJSON = {
+    email: "sharkman@oceanmail.com",
+    pwd: "*****",
+    phone: "123-456-7890"
+  };
+
+  $("#loadData").click(() => {
+
+    $("#email").val(userJSON.email);
+
+    $("#pwd").val(userJSON.pwd);
+
+    $("#phone").val(userJSON.phone);
+  });
 
 
-
-  /*
-  get a button taking it to somewhere
-  ex. select state, takes you to counties in that state
-  */
+  // getting submit button after verification
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("submitButton");
   
-// input value from submit bar
-  var el = document.getElementById("submitButton");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
 
-// click event on that element, run named f/n
-el.addEventListener("click", displayuserEmail);
+  
+// // input value from submit bar
+//   var el = document.getElementById("submitButton");
 
-function displayuserEmail() {
-  // grab the value of the user input
-  var elName = document.getElementById("userEmail");
-  //do something with it
-// need to put info somewhere??
-}
+// // click event on that element, run named f/n
+// el.addEventListener("click", displayuserEmail);
 
-});
+// function displayuserEmail() {
+//   // grab the value of the user input
+//   var elName = document.getElementById("userEmail");
+//   //do something with it
+// // need to put info somewhere??
+// }
+
