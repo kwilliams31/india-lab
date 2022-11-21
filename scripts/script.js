31fcbaa1f9c00f33da7696ccb91ac6ca29bee406
Kristina <111464160+kwilliams31@users.noreply.github.com>
2022-11-21 11:57:33 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -64,6 +64,46 @@ function myFunction() {
     }
   }
 
+  // radio button for dependant input
+  $("input[type=radio]").on("change", function () {
+
+    let radioChoice = $("input[type=radio]:checked").val(); // Yes or No
+    let newFishList = getFish(radioChoice);
+    console.log(newFishList);
+
+    let optionList = "";
+
+    for (i = 0; i < newFishList.length; i++) {
+      optionList += `<option value=" ${newFishList[i]} ">${newFishList[i]} </option>`;
+    }
+    $("#fishSelect").empty().append(optionList);
+  });
+
+  $("#submitForm").on("click", (e) => {
+    e.preventDefault();
+
+    // get data from an input
+
+    let dataStuff = `{ "letterSelected:" " ${$(
+      "input[type=radio]:checked"
+    ).val()} " }`;
+
+    console.log(dataStuff);
+  });
+
+function getFish(fishType) {
+// getting array list if user checks yes
+  let yesArray = ["Manta Ray", "Seahorse", "Parrot Fish", "Shark", "Other"];
+  let noArray= ["Thank you", "I would like to see a certain plant"]
+
+  if (fishType === "yes") {
+    return yesArray; }
+  else if (fishType === "no") {
+    return noArray; } 
+  else {
+    return ["Thank you"];
+  }
+}
   
 // // input value from submit bar
 //   var el = document.getElementById("submitButton");
